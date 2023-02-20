4c4cb9a54a800a00f3276b85d303e0808c368adc
Ryan Day <rddays@gmail.com>
2023-02-20 17:51:01 -0600

    Add options for sanitizers

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,5 +1,55 @@
-function(enable_sanitizers project_name)
-  set(LIST_OF_SANITIZERS address,undefined)
-  target_compile_options(${project_name} PRIVATE -fsanitize=${LIST_OF_SANITIZERS})
-  target_link_options(${project_name} PRIVATE -fsanitize=${LIST_OF_SANITIZERS})
+function(
+  enable_sanitizers
+  _project_name
+  ENABLE_SANITIZER_ADDRESS
+  ENABLE_SANITIZER_LEAK
+  ENABLE_SANITIZER_UNDEFINED_BEHAVIOR
+  ENABLE_SANITIZER_THREAD
+  ENABLE_SANITIZER_MEMORY)
+
+  set(SANITIZERS "")
+
+  if(${ENABLE_SANITIZER_ADDRESS})
+    list(APPEND SANITIZERS "address")
+  endif()
+
+  if(${ENABLE_SANITIZER_LEAK})
+    list(APPEND SANITIZERS "leak")
+  endif()
+
+  if(${ENABLE_SANITIZER_UNDEFINED_BEHAVIOR})
+    list(APPEND SANITIZERS "undefined")
+  endif()
+
+  if(${ENABLE_SANITIZER_THREAD})
+    if("address" IN_LIST SANITIZERS OR "leak" IN_LIST SANITIZERS)
+      message(WARNING "Thread sanitizer does not work with Address or Leak sanitizer enabled")
+    else()
+      list(APPEND SANITIZERS "thread")
+    endif()
+  endif()
+
+  if(${ENABLE_SANITIZER_MEMORY} AND CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    message(
+      WARNING
+        "Memory sanitizer requires all the code (including libc++) to be MSan-instrumented otherwise it reports false positives"
+    )
+    if("address" IN_LIST SANITIZERS
+       OR "thread" IN_LIST SANITIZERS
+       OR "leak" IN_LIST SANITIZERS)
+      message(WARNING "Memory sanitizer does not work with Address, Thread or Leak sanitizer enabled")
+    else()
+      list(APPEND SANITIZERS "memory")
+    endif()
+  endif()
+
+  list( JOIN SANITIZERS "," LIST_OF_SANITIZERS)
+
+  if(LIST_OF_SANITIZERS)
+    if(NOT "${LIST_OF_SANITIZERS}" STREQUAL "")
+      target_compile_options(${_project_name} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+      target_link_options(${_project_name} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+    endif()
+  endif()
+
 endfunction()
